da49fc97d0aa8ee3f26466d7faaeaf0385d548cf
exersalza <squidodle44@gmail.com>
2022-06-13 14:07:06 +0200

    Can i now push stuff???????

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<p>luLw</p>);
+root.render(<p>luLw</p>); // can i now push stuff?
