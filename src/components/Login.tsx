5e868b67d9a4150014468ea39301a835003b2102
exersalza <squidodle44@gmail.com>
2022-06-13 21:23:50 +0200

    Dev branch update

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function Login() {
     const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
 
     function submitHandler(event: { preventDefault: () => any; }) {
         event.preventDefault()
@@ -9,13 +10,14 @@ function Login() {
     }
 
     return (
-        <div>
-            <h1>Hello :)</h1>
-            <form onSubmit={submitHandler}>
-                <label>
-                    Username
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
-                </label>
+        <div className='w-full max-w-xs bg-accent'>
+            <h1 className='title'>Kenexar</h1>
+            <form onSubmit={submitHandler} className='bg-secondary rounded px-8 pt-6 pb-8 mb-4'>
+                <label htmlFor='username'>Username</label>
+                <input type="text" name='username' value={name} onChange={(e) => setName(e.target.value)}/>
+
+                <label htmlFor='username'>Password</label>
+                <input type="password" name='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
             </form>
         </div>
     )
